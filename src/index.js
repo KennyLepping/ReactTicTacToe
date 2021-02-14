fe8e27c25fa2b911486a57258a2f9c9341b39e7a
Kenny Lepping <kennylepping@gmail.com>
2021-02-14 02:43:16 -0500

    Tabbed in the Board prop

--- src/index.js
+++ src/index.js
@@ -109,10 +109,10 @@ class Game extends React.Component {
         return (
         <div className="game">
             <div className="game-board">
-            <Board
-                squares={current.squares}
-                onClick={i => this.handleClick(i)}
-            />
+                <Board
+                    squares={current.squares}
+                    onClick={i => this.handleClick(i)}
+                />
             </div>
             <div className="game-info">
             <div>{status}</div>
